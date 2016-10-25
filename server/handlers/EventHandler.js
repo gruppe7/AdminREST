4e938ea747837e2698e0019987b6e8cfe97e6695
gunnarkd <gunnarkd@student.hist.no>
2016-10-25 10:52:58 +0200

    cleanup before merging

--- server/handlers/EventHandler.js
+++ server/handlers/EventHandler.js
@@ -9,12 +9,11 @@ var salt = bcrypt.genSaltSync(10);
 
 var EventHandler = function() {
   this.events=handleEventsRequest;
-  this.attend=handleAttendingRequest;
+
 };
 
 
 function handleEventsRequest(req, res){
-
   var sql = "SELECT Events.*, count(EventParticipation.username) as attending FROM Events left join EventParticipation on Events.eventId=EventParticipation.eventId group by Events.eventId";
   pool.getConnection().then(
     function (connection){
@@ -35,36 +34,7 @@ function handleEventsRequest(req, res){
   );
 }
 
-function handleAttendingRequest(req, res){
-  var semester=req.params.semester || null;
 
-  if(semester==null ){
-    res.json(400, 'semester not submitted');
-    return;
-  }
-
-  var sql = "SELECT Lockers.lockerId, Lockers.floor FROM Lockers WHERE Lockers.lockerId NOT IN (SELECT LockerRent.lockerId FROM LockerRent where LockerRent.semester=?)";
-  var inserts = [semester];
-  sql=mysql.format(sql, inserts);
-
-  pool.getConnection().then(
-    function (connection){
-      connection.query(sql).then(
-        function (rows){
-          console.log(rows);
-          res.json(201, rows);
-          return;
-        },
-        function (err){
-          res.json(500, {error:'something went wrong while getting db connection'});
-        }
-      )
-    },
-    function (err){
-      res.json(500, {error:'could not connect to database'});
-    }
-  );
-}
 
 
 
--- server/test/user-test.js
+++ server/test/user-test.js
@@ -26,27 +26,25 @@ describe('Routing', function() {
           done();
         });
     });
-    /*it('should correctly update an existing account', function(done){
-	var body = {
-		firstName: 'JP',
-		lastName: 'Berd'
-	};
-	request(url)
-		.put('/api/profiles/vgheri')
-		.send(body)
-		.expect('Content-Type', /json/)
-		.expect(200) //Status code
-		.end(function(err,res) {
-			if (err) {
-				throw err;
-			}
-			// Should.js fluent syntax applied
-			res.body.should.have.property('_id');
-	                res.body.firstName.should.equal('JP');
-	                res.body.lastName.should.equal('Berd');
-	                res.body.creationDate.should.not.equal(null);
-			done();
-		});
-	});*/
+    it('should return return privilegies and access token when using correct username and password', function(done) {
+      var login = {
+        username: 'gunnadal',
+        password: 'abc123'
+      };
+    request(url)
+	.post('/users/')
+	.send(login)
+    // end handles the response
+	.end(function(err, res) {
+          if (err) {
+            throw err;
+          }
+          res.should.have.property('status',201);
+          res.body.eventmanager.should.equal(1);
+          res.body.employee.should.equal(1);
+          res.body.token.should.not.equal(null);
+          done();
+        });
+    });
   });
 });
