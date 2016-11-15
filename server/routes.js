1d595b9b5fa59026f5cee1086afce7fcd6425323
gunnarkd <gunnarkd@student.hist.no>
2016-11-15 10:00:05 +0100

    automated install

--- server/routes.js
+++ server/routes.js
@@ -2,9 +2,21 @@
 
 function setup(app, handlers) {
 
-  app.get('/lockers/:semester', handlers.lockers.lockers);
-  //app.post('/lockers/:lockerId', handlers.lockers.reserve);
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Max-Age", "1728000");
 
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+
+  });
+  app.get('/lockers/:semester', handlers.lockers.lockers);
   app.post('/users/',handlers.users.login);
   app.get('/events/', handlers.events.events);
   app.post('/students/', handlers.students.new);
