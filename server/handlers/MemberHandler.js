a973f426270a73db2311070f9c5ee95266aed494
gunnarkd <gunnarkd@student.hist.no>
2016-11-24 11:21:34 +0100

    unused import removed

--- server/handlers/MemberHandler.js
+++ server/handlers/MemberHandler.js
@@ -5,7 +5,6 @@ var input = require('../utils/input.js');
 
 var mailer = require('nodemailer-promise');
 var sendEmail= mailer.config(config.mailer);
-
 var pool = mysql.createPool(config.db.mysql);
 
 var salt = bcrypt.genSaltSync(10);
